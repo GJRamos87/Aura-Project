a982abf1b633f40fa7183eec12f62121ef0d323a
George J Ramos <gjramos87@gmail.com>
2015-08-26 11:24:26 -0400

    text

--- app/assets/javascripts/turbolinks_transitions.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('page:change', function() {
-        document.getElementsByClassName('container-fluid').className += 'animated fadeIn';
-});
-document.addEventListener('page:fetch', function() {
-        document.getElementsByClassName('container-fluid').className += 'animated fadeOut';
-});
