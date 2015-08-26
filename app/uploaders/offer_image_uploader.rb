56040ea15a9b583ec8d8ac6d2f8415aebf371bb8
George J Ramos <gjramos87@gmail.com>
2015-08-26 14:58:14 -0400

    added u know part 2

--- app/uploaders/offer_image_uploader.rb
+++ app/uploaders/offer_image_uploader.rb
@@ -18,7 +18,7 @@ class OfferImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    ActionController::Base.helpers.asset_path([version_name, "happy.jpeg"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("assets/" + [version_name, "happy.jpeg"].compact.join('_'))
   end
 
   # Process files as they are uploaded:
